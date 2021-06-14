12688b6b6e8a8f3d7869e280360c8cf2b6574744
furkanogur <20191132035@ogr.akdeniz.edu.tr>
2021-06-14 16:35:26 +0300

    kategori ürün listeleme ilk aşama  14.06.2021 // 16:35

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,7 +2,17 @@ import { ConfirmDialogComponent } from './../dialogs/confirm-dialog/confirm-dial
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Sonuc } from './../../models/Sonuc';
 import { MyAlertService } from './../../services/myAlert.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Kategoriler } from 'src/app/models/Kategori';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from 'src/app/services/api.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map, shareReplay } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 
 @Component({
   selector: 'app-home',
@@ -10,34 +20,65 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  dialogref:MatDialogRef<ConfirmDialogComponent>
-  constructor(
-    public alert:MyAlertService,
-    public matDialog:MatDialog
-    ) { }
-  
+  uyeAdi: string;
+  kategoriler: Kategoriler[];
+  kategoriById:string;
+  secKategori: Kategoriler[];
+  kategoriId:string;
+  frm:FormGroup;
+  dataSource: any;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  displayedColumns = ['Adi', 'Aciklama', 'Fiyat']
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver,
+    public apiServis: ApiService,
+    public route: ActivatedRoute,
+    public matDialog: MatDialog,
+    public alert: MyAlertService,
+    public frmbuilder: FormBuilder,
+  ) {
+    this.frm = new FormGroup({
+      kategoriId: new FormControl(),
+    })
+    console.log(this.kategoriId)
+  }
 
   ngOnInit() {
+    if (this.apiServis.oturumKontrol) {
+      this.uyeAdi = localStorage.getItem("uyeadi");
+    }
+    this.KategoriListele();
+    console.log(this.kategoriId)
+
+  }
+  OturumKapat() {
+    localStorage.clear();
+    location.href = ("/");
   }
 
-  AlertAc(p:boolean){
-    var s:Sonuc= new Sonuc();
-    s.islem =p;
-    s.mesaj="Alert Test Mesajı";
-    this.alert.AlertUygula(s)
+  KategoriListele() {
+    this.apiServis.KategoriListe().subscribe((d: any = Kategoriler) => {
+      this.kategoriler = d;
+    })
+  }
+  KategoriUrunById(akategoriId:string) {
+    this.apiServis.KategoriUrunListe(akategoriId).subscribe((d:any=Kategoriler) =>{
+      this.secKategori = d;
+      this.dataSource = new MatTableDataSource(d);
+      this.dataSource.sort = this.sort
+
+      console.log(this.secKategori);
+    })
   }
 
-  ConfirmAc(){
-this.dialogref=this.matDialog.open(ConfirmDialogComponent,{
-  width:'400px'
-});
-this.dialogref.componentInstance.dialogMesaj="Kayıt Silinecektir Onaylıyor musunuz?";
-
-this.dialogref.afterClosed().subscribe(d=>{
-  console.log(d)
-  if(d){
-    //silme rutine
-  }
-})
+  katıdgetir(kategoriId:string){
+    console.log(kategoriId);
   }
 }
+
--- src/app/components/main-nav/main-nav.component.ts
+++ src/app/components/main-nav/main-nav.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Kategoriler } from './../../models/Kategori';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Component, ViewChild } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { ApiService } from 'src/app/services/api.service';
+import { ActivatedRoute } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { MyAlertService } from 'src/app/services/myAlert.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-main-nav',
@@ -10,7 +18,16 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./main-nav.component.css']
 })
 export class MainNavComponent {
-uyeAdi:string;
+  uyeAdi: string;
+  kategoriler: Kategoriler[];
+  kategoriById:string;
+  secKategori: Kategoriler[];
+  kategoriId:string;
+  frm:FormGroup;
+  dataSource: any;
+  @ViewChild(MatSort) sort: MatSort;
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  displayedColumns = ['Adi', 'Aciklama', 'Fiyat']
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
@@ -18,17 +35,47 @@ uyeAdi:string;
     );
 
   constructor(private breakpointObserver: BreakpointObserver,
-    public servis:ApiService
-    ) {}
+    public apiServis: ApiService,
+    public route: ActivatedRoute,
+    public matDialog: MatDialog,
+    public alert: MyAlertService,
+    public frmbuilder: FormBuilder,
+  ) {
+    this.frm = new FormGroup({
+      kategoriId: new FormControl(),
+    })
+    console.log(this.kategoriId)
+  }
 
-ngOnInit(): void {
- if(this.servis.oturumKontrol){
-this.uyeAdi = localStorage.getItem("uyeadi");
- }
-  
-}
-    OturumKapat() {
-      localStorage.clear();
-      location.href = ("/");
+  ngOnInit() {
+    if (this.apiServis.oturumKontrol) {
+      this.uyeAdi = localStorage.getItem("uyeadi");
     }
+    this.KategoriListele();
+    console.log(this.kategoriId)
+
+  }
+  OturumKapat() {
+    localStorage.clear();
+    location.href = ("/");
+  }
+
+  KategoriListele() {
+    this.apiServis.KategoriListe().subscribe((d: any = Kategoriler) => {
+      this.kategoriler = d;
+    })
+  }
+  KategoriUrunById(akategoriId:string) {
+    this.apiServis.KategoriUrunListe(akategoriId).subscribe((d:any=Kategoriler) =>{
+      this.secKategori = d;
+      this.dataSource = new MatTableDataSource(d);
+      this.dataSource.sort = this.sort
+
+      console.log(this.secKategori);
+    })
+  }
+
+  katıdgetir(kategoriId:string){
+    console.log(kategoriId);
+  }
 }
